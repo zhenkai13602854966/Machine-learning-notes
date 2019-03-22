fb9bab9e6f36330a9d1c61b8a51ea3cb45c17ca8
zhenkai13602854966 <13602854966@163.com>
2019-03-22 16:35:50 +0800

    Create chap1.py

---
+++ chap1.py
@@ -0,0 +1,31 @@
+#!/usr/local/bin/python3.6
+# _*_ coding:UTF-8 _*_
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+
+HOUSING_PATH = os.path.join("datasets", "housing")
+
+
+# define a function to load data
+def load_housing_data(housing_path=HOUSING_PATH):
+    cvs_path = os.path.join(housing_path, "housing.csv")
+    return pd.read_csv(cvs_path)
+
+
+def save_fig(fig_id, tight_layout=True):
+    path = os.path.join(HOUSING_PATH, "images", fig_id + ".png")
+    print("Saving figure", fig_id)
+    if tight_layout:
+        plt.tight_layout()
+    plt.savefig(path, format='png', dpi=300)
+
+
+housing = load_housing_data()     # 调用函数
+# print(housing)
+# print(housing.head())           # 显示前五行数据
+# print(housing.info())           # 快速获取数据集的简答描述
+# print(housing.describe())       # 显示数值属性摘要
+housing.hist(bins=50, figsize=(20, 15))
+save_fig("attribute_histogram_plots")
+plt.show()
